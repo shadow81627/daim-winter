bb1beb668ca4fcb90fbdbf6a6f47a233b7033e1a
Damien Robinson <damien.robinson@pocketpasta.com>
2022-04-23 18:52:49 +1000

    chore(release tasks): remove down message

--- release-tasks.sh
+++ release-tasks.sh
@@ -4,7 +4,7 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down) || true
     # Update codebase
     git fetch origin main
     git reset --hard origin/main
