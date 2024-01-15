247b5d1cc54e9abab6774c7d2cf564208901c973
Damien Robinson <damien.robinson@daim.dev>
2024-01-15 13:51:10 +1000

    chore: add tenant seeder

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -6,8 +6,9 @@ use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
-    use HasDatabase, HasDomains;
+    use HasFactory, HasDatabase, HasDomains;
 }
---
+++ database/seeders/TenantSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Tenant;
+
+class TenantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Tenant::factory()->count(2)->create();
+    }
+}
